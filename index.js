3b91c67fc89e23b5b82c40dbc19991e2a98ec94e
asta-otaku <afolabiibrahim08@gmail.com>
2024-03-25 12:22:27 +0100

    Vercel config

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
+// app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("trust proxy", 1);
 app.use(
